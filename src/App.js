9a6d60d3423b610e794558a2240719f085d27833
hachishti <34640012+hachishti@users.noreply.github.com>
2018-01-01 22:52:35 +0400

    IncludeCSS ver 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import './App.css'
 class HelloworlsReactJsx extends Component{
+  
+  customLinkStyle = {
+    backgroundColor: 'lightblue', 
+    textDecoration: 'underline'
+  }
+  
   render(){
     return(
-      <a href={this.props.link}>{this.props.linkText}</a>
+      <a href={this.props.link} style={this.customLinkStyle}>{this.props.linkText}</a>
     );
   }
 }
