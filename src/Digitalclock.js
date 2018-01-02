4a264d57a8f447a12bcb459fb45d9cc9146f56c2
hachishti <34640012+hachishti@users.noreply.github.com>
2018-01-02 23:48:33 +0400

    UseReactStates v1
    In this versionn we used states of React JSX

---
+++ src/Digitalclock.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css'
+
+class Clock extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentTime: new Date().toLocaleString()
+        }
+        this.updateTime();
+    }
+
+    updateTime() {
+        setInterval(() => {
+            this.setState(
+                {
+                    currentTime: new Date().toLocaleString()
+                }
+            )
+        }, 1000)
+    }
+
+    render() {
+        return (
+            <h1>{this.state.currentTime}</h1>
+        )
+    }
+}
+
+export default Clock;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';   
-import HelloWorldJsx from './App';
+import HelloWorldJsx from './Helloworld';
 import TestMethod from './Methods';
+import Clock from './Digitalclock';
 let varmain = document.getElementById('root');
 ReactDOM.render(
     <div>
         <HelloWorldJsx link='https://gmail.com' linkText='GMAIL'/>
         <HelloWorldJsx link='https://yahoo.com' linkText='YAHOO' />
         <TestMethod />
+        <Clock />
     </div>
 ,varmain);
